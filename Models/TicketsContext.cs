71111a6414a6559d33d685dad24b54808cc05130
Sergio Monteros <sergionexx1@gmail.com>
2020-10-22 23:39:26 -0500

    first commit

---
+++ Models/TicketsContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Crud.Models
+{
+    public partial class TicketsContext : DbContext
+    {
+        public TicketsContext()
+        {
+        }
+
+        public TicketsContext(DbContextOptions<TicketsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cedula)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+                entity.Property(e => e.Cola)
+                   .HasMaxLength(50)
+                   .IsUnicode(false);
+
+                entity.Property(e => e.Estado)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+              
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Crud.Models
+{
+    public partial class Usuarios
+    {
+        public int Id { get; set; }
+
+        [Required]        
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+     
+        public string Estado { get; set; }
+
+        [Required]
+        public string Cedula { get; set; }
+
+      
+        public string Cola { get; set; }
+
+       
+    }
+}
